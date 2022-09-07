cb201669d794a9d4fba8c4a006cfd0883c2153ae
vlad moryakov <sat1sfy@vk.com>
2022-09-07 17:21:10 +0300

    fixes

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -15,18 +15,21 @@ def test_download_pics():
     with requests_mock.Mocker() as m:
         test_page = open(f'{FIXTURES_PATH}/test_page.html', 'r').read()
         m.get(TEST_URL, text=test_page)
-        paths = (f'tests/fixtures/pic1.png',
-                 f'tests/fixtures/pic2.jpg',
-                 f'tests/fixtures/pics/pic3.png')
+        paths = ('tests/fixtures/pic1.png',
+                 'tests/fixtures/pic2.jpg',
+                 'tests/fixtures/pics/pic3.png')
         for path in paths:
             with open(path, 'rb') as pic:
                 tmp = pic.read()
             m.get(f'{TEST_URL}{path}', content=tmp)
         with tempfile.TemporaryDirectory() as tmp_dir:
             html_file = downloader.download('https://testing.ru/', tmp_dir)
-            new_dir = f'{tmp_dir}/{make_path(TEST_URL, dir=True)}'
+            local_path_url = make_path(TEST_URL, dir=True)
+            new_dir = f'{tmp_dir}/{local_path_url}'
             for path in paths:
-                local_path = f'{make_path(TEST_URL)}-{make_path(path, file=True)}'
+                path_url = make_path(TEST_URL)
+                path_file = make_path(path, file=True)
+                local_path = f'{path_url}-{path_file}'
                 path_to_file = f'{new_dir}/{local_path}'
                 with open(path, 'rb') as file:
                     expected = file.read()
@@ -42,17 +45,20 @@ def test_download_link_scripts():
     with requests_mock.Mocker()as m:
         test_page = open(f'{FIXTURES_PATH}/test_scr_link.html', 'r').read()
         m.get(TEST_URL, text=test_page)
-        paths = (f'tests/fixtures/links/link1.css',
-                 f'tests/fixtures/scripts/script1.js',)
+        paths = ('tests/fixtures/links/link1.css',
+                 'tests/fixtures/scripts/script1.js',)
         for path in paths:
             with open(path, 'rb') as file:
                 tmp = file.read()
             m.get(f'{TEST_URL}{path}', content=tmp)
         with tempfile.TemporaryDirectory() as tmp_dir:
             downloader.download('https://testing.ru', tmp_dir)
-            new_dir = f'{tmp_dir}/{make_path(TEST_URL, dir=True)}'
+            local_path_url = make_path(TEST_URL, dir=True)
+            new_dir = f'{tmp_dir}/{local_path_url}'
             for path in paths:
-                local_path = f'{make_path(TEST_URL)}-{make_path(path, file=True)}'
+                path_url = make_path(TEST_URL)
+                path_file = make_path(path, file=True)
+                local_path = f"{path_url}-{path_file}"
                 path_to_file = f'{new_dir}/{local_path}'
                 with open(path, 'rb') as file:
                     expected = file.read()
